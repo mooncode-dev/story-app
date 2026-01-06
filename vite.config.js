431a8905f80631b1559deeba529a52ea9db142cf
Selomitha Nong <nongselomitha@gmail.com>
2026-01-07 00:25:26 +0800

    fix asset paths in index.html and base config in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/story-app/',
+  
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src', 'public'),
   build: {
